092b085ff6ace533b7ae199757e4edd0a2fee89c
zmiarzynski <56540726+zmiarzynski@users.noreply.github.com>
2020-04-15 22:56:45 +0200

    Add files via upload

---
+++ calculationsvol2.py
@@ -0,0 +1,52 @@
+from node import Node
+
+
+class TreeCalculations:
+
+    def __init__(self):
+        self.numbers = []
+
+    def get_sum(self, node):
+        if isinstance(node, Node):
+            self.numbers.clear()
+            amount = 0
+            self.add_node_to_list(node)
+            for i in range(len(self.numbers)):
+                amount += self.numbers[i]
+            return amount
+        else:
+            raise TypeError("Node type required")
+
+    def get_mean(self, node):
+        if isinstance(node, Node):
+            amount = self.get_sum(node)
+            return amount/len(self.numbers)
+        else:
+            raise TypeError("Node type required")
+
+    def get_median(self, node):
+        if isinstance(node, Node):
+            self.numbers.clear()
+            self.add_node_to_list(node)
+            self.numbers.sort()
+            if len(self.numbers) % 2 == 0:
+                temp = self.numbers[len(self.numbers) // 2] + self.numbers[(len(self.numbers) // 2) - 1]
+                return temp / 2
+            else:
+                return self.numbers[(len(self.numbers) - 1) // 2]
+        else:
+            raise TypeError("Node type required")
+
+    def add_node_to_list(self, node):
+        if node is not None:
+            self.numbers.append(node.value), self.add_node_to_list(node.right), self.add_node_to_list(node.left)
+        else:
+            pass
+
+
+
+
+
+
+
+
